260a5b0ec41519bc0e02117814db095f82c5fe66
pfusik <pfusik@edcfc96e-596b-e111-b49a-002618dea8cf>
2012-11-28 14:56:48 +0000

    STIL: fixed and the simple tool i used for it.

---
+++ trunk/tools/validate-stil.bat
@@ -0,0 +1,4 @@
+grep -E " $" ../asma/Docs/STIL.txt
+grep -E ".{100}" ../asma/Docs/STIL.txt
+grep -v -n -E "^#|^/|^   NAME: |^  TITLE: |^ ARTIST: |^COMMENT: |^         |^$|\(#[0-9]+\)$" ../asma/Docs/STIL.txt
+perl -nle "m{^/} and (-e qq{../asma/$_} or print)" ../asma/Docs/STIL.txt
