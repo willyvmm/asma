3d41b71a635f9321db21a1e7293da152c76eb0cb
pfusik <pfusik@edcfc96e-596b-e111-b49a-002618dea8cf>
2012-12-24 12:59:00 +0000

    STIL: define number of entries as the number of slashes in the first column.

--- trunk/tools/validate-stil.bat
+++ trunk/tools/validate-stil.bat
@@ -2,3 +2,4 @@ grep -E " $" ../asma/Docs/STIL.txt
 grep -E ".{100}" ../asma/Docs/STIL.txt
 grep -v -n -E "^#|^/|^   NAME: |^  TITLE: |^ ARTIST: |^COMMENT: |^         |^$|\(#[0-9]+\)$" ../asma/Docs/STIL.txt
 perl -nle "m{^/} and (-e qq{../asma/$_} or print)" ../asma/Docs/STIL.txt
+grep -c "^/" ../asma/Docs/STIL.txt
